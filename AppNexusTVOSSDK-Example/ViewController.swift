5ddfb97901c38cbb4565935bc3994d46d05775a7
David Cordero <david@corderoramirez.com>
2018-02-22 11:21:57 +0100

    Add playground project

---
+++ AppNexusTVOSSDK-Example/ViewController.swift
@@ -0,0 +1,59 @@
+import AVKit
+import AVFoundation
+import UIKit
+import ANTVSDK
+
+private let appNexusPsetId = 12345
+
+class ViewController: UIViewController, VMAPPlaybackControllerProtocol {
+
+    private var playerViewController: AVPlayerViewController!
+    private var adPlaybackController: VMAPPlaybackController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+        setUpAppNexusSDK()
+        
+        setUpStreamToPlay(url: URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!)
+    }
+    
+    // MARK: - VMAPPlaybackControllerProtocol
+    
+    func adPlaybackControllerDidPrepare(result: Bool) {
+        if result {
+            adPlaybackController?.play()
+        } else {
+            playerViewController.player?.play()
+        }
+    }
+        
+    // MARK: - Private
+    
+    private func setUpView() {
+        playerViewController = AVPlayerViewController()
+        playerViewController.view.frame = view.bounds
+        playerViewController.player = AVPlayer()
+        
+        addChildViewController(playerViewController)
+        view.addSubview(playerViewController.view)
+    }
+    
+    private func setUpAppNexusSDK() {
+        adPlaybackController = VMAPPlaybackController()
+        adPlaybackController.isSkippable = true
+        
+        adPlaybackController.setup(appNexusPsetId: appNexusPsetId,
+                                   contentVideoPlayerViewController: playerViewController,
+                                   contentVideoPlayer: playerViewController.player!,
+                                   contentUIViewController: self,
+                                   delegate: self)
+        
+    }
+    
+    private func setUpStreamToPlay(url: URL) {
+        let asset = AVURLAsset(url: url, options: nil)
+        let playerItem = AVPlayerItem(asset: asset, automaticallyLoadedAssetKeys: nil)
+        playerViewController?.player?.replaceCurrentItem(with: playerItem)
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# AppNexusTVOSSDK-Playground
+# AppNexusTVOSSDK Playground
+
+This repo contains the result of testing the usage of the framework AppNexusTVOSSDK in playground mode...
+
+### Notes:
+- The frameworks seems to be connecting to a unsecure domain (http://ib.adnxs.com). So, it requires to definition of an exception in TLS settings in the Info.plist.
+- Console is full of logs, I did not find yet a way to make it less verbose. To check: is there sensitive information, as the tracking pset id, shown in the logs?
+- Some logs regarding TVJS appears in the console. To check: is there any conflict with the js application context when using the framework in an hybrid (TVMLKit+UIKit) application?
+- To check, how is the framework managing the changes in the rate of the bound AVPlayer? what happens when having a KVO to "rate" keeping track of the player status?
+- To check, what happens with the ads when having modals presented over the player?
